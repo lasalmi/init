2442ad1aa51c027e2ecb8047f50b929e423dd393
Lauri Salmi <late.salmi@gmail.com>
2022-09-30 12:39:27 +0300

    Adding own script

---
+++ scripting/03.sh
@@ -0,0 +1,31 @@
+echo "Please make your tunnel selection:"
+echo "1)To Apache"
+echo "2)To Deluge"
+echo "3)Both"
+while :; do
+	read -rsn1 selection
+  case "$selection" in 
+    1|2|3) break ;;
+    *) ;;
+  esac
+done
+
+if [ "$selection" == "1" ]; then
+	if [ ssh -N -L 8181:myexample.com:80 user@myexample.com & ]; then
+		echo "Tunnel to Apache failed!"
+	else
+		echo "Tunnel to Apache port 80 established on port 8181"
+	fi
+
+elif [ "$selection" == "2" ]; then
+	if [ ssh -N -L 8190:myexample.com:8112 user@myexample.com & ]; then
+		echo "Tunnel to Deluge failed!"
+	else
+		echo "Tunnel to Deluge port 8112 established on port 8190"
+	fi
+
+elif [ "$selection" == "3" ]; then
+	ssh -N -L 8190:myexample.com:8112 user@myexample.com &
+	ssh -N -L 8181:myexample.com:80 user@myexample.com &
+	echo "Tunnel to Apache at 8181 and to Deluge at 8190"
+fi
