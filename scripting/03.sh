df8f86fc904bcb6a27cb97320a4c20569b2440b0
Lauri Salmi <lasalmi@student.hive.fi>
2022-09-30 12:59:09 +0300

    ..slight change

--- scripting/03.sh
+++ scripting/03.sh
@@ -11,14 +11,14 @@ while :; do
 done
 
 if [ "$selection" == "1" ]; then
-	if [ ssh -N -L 8181:localhost:80 debian@localhost & ]; then
+	if [ ssh -N -L 8181:localhost:80 debian@localhost & > 0]; then
 		echo "Tunnel to Apache failed!"
 	else
 		echo "Tunnel to Apache port 80 established on port 8181"
 	fi
 
 elif [ "$selection" == "2" ]; then
-	if [ ssh -N -L 8190:localhost:8112 debian@localhost & ]; then
+	if [ ssh -N -L 8190:localhost:8112 debian@localhost & > 0]; then
 		echo "Tunnel to Deluge failed!"
 	else
 		echo "Tunnel to Deluge port 8112 established on port 8190"
